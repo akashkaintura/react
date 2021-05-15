dbdf64295320078db997cea4ca2db880a0245bc0
Akash <cole.akash@Akash.local>
2021-05-15 21:16:36 +0530

    update: tailwind CSS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import "./assests/css/style.css"
 
-function App() {
+function App({ title }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-gray-600 text-white p-5 border">
+      <div> {title}</div>
     </div>
-  );
+  )
 }
-
-export default App;
+export default App
