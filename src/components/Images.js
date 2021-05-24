5912e81ec1094b5732512b08441df75fef5e1f5f
Akash <cole.akash@Akash.local>
2021-05-24 21:56:50 +0530

    update: indepth useEffect

--- src/components/Images.js
+++ src/components/Images.js
@@ -1,41 +1,62 @@
-import React, { Component } from "react"
+import React, { useEffect } from "react"
 
-class Images extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { interval: null }
-  }
+export default function Images() {
+  // const [myInterval, setmyInterval] = useState(null)
 
-  // component Mount
-  componentDidMount() {
-    console.log("Images Comp Mount")
-    this.setState({
-      interval: setInterval(() => {
-        console.log("hello")
-      }, 1000),
-    })
-  }
-  // component UnMount
-  componentWillUnmount() {
-    console.log("Images UnMounted")
-    clearInterval(this.state.interval)
-  }
+  useEffect(() => {
+    console.log("Images Component Mounted")
+    const interval = setInterval(() => {
+      console.log("Hi")
+    }, 1000)
 
-  componentDidUpdate() {
-    console.log("App Update")
-  }
+    return () => clearInterval(interval)
+  }, [])
 
-  render() {
-    return (
-      <div>
-        <img
-          className="my-2"
-          src="https://images.unsplash.com/photo-1621084556062-c2d810daff25?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"
-          alt="cycle"
-        />
-      </div>
-    )
-  }
+  return (
+    <img
+      className="my-2"
+      src="https://images.unsplash.com/photo-1621084556062-c2d810daff25?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"
+      alt="cycle"
+    />
+  )
 }
 
-export default Images
+// class Images extends Component {
+//   constructor(props) {
+//     super(props)
+//     this.state = { interval: null }
+//   }
+
+//   // component Mount
+//   componentDidMount() {
+//     console.log("Images Comp Mount")
+//     this.setState({
+//       interval: setInterval(() => {
+//         console.log("hello")
+//       }, 1000),
+//     })
+//   }
+//   // component UnMount
+//   componentWillUnmount() {
+//     console.log("Images UnMounted")
+//     clearInterval(this.state.interval)
+//   }
+
+//   componentDidUpdate() {
+//     console.log("App Update")
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <img
+//           className="my-2"
+//           src="https://images.unsplash.com/photo-1621084556062-c2d810daff25?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"
+//           alt="cycle"
+//         />
+//       </div>
+//     )
+//   }
+// }
+
+// export default Images
