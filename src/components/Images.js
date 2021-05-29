fa56d434d8eb68eef05be4b78cc4f4bc6e0f854d
Akash <cole.akash@Akash.local>
2021-05-29 20:57:40 +0530

    fix: add new images state and spread operator usage

--- src/components/Images.js
+++ src/components/Images.js
@@ -1,17 +1,17 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 
 export default function Images() {
-  const Images = [
+  const [images, setImages] = useState([
     "https://images.unsplash.com/photo-1622241944227-ae279379cc80?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
     "https://images.unsplash.com/photo-1622227432807-91af5901f941?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=616&q=80",
     "https://images.unsplash.com/photo-1622281549424-fd9aaea1fd43?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
     "https://images.unsplash.com/photo-1621570169561-0c2a2e193ee1?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
     "https://images.unsplash.com/photo-1622228262959-8cead91ed5c2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
     "https://images.unsplash.com/photo-1622279240815-20a1d7449e40?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=892&q=80",
-  ]
+  ])
 
   function ShowImage(params) {
-    return Images.map((image) => {
+    return images.map((image) => {
       return (
         <div>
           <img src={image} width="150px" alt="unsplash" />
@@ -20,10 +20,27 @@ export default function Images() {
     })
   }
 
+  function handleAdd() {
+    let temp = [...images]
+
+    temp.push(
+      "https://images.unsplash.com/photo-1621602071737-e267c09db65e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80"
+    )
+    console.log("working")
+
+    setImages(temp)
+  }
+
   return (
     <section>
       <div className="flex justify-center">
         <ShowImage />
+      </div>
+      <div className="flex justify-between my-5">
+        <input type="text" className="p-2 border-gray-800 shadow rounded" />
+        <button className="p-2 bg-green-600 text-white" onClick={handleAdd}>
+          Add New
+        </button>
       </div>
     </section>
   )
